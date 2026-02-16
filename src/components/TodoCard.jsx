00c411e213bfad185d22c7952c40db022243a264
Manash <rinpratim0635@gmail.com>
2026-02-17 01:11:27 +0530

    made todo-app in react

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,25 @@
+export function TodoCard(props) {
+  const { todo, handleDeleteTodo, todoIndex, handleCompletetodo } = props;
+  return (
+    <div className="card todo-item">
+      <p>{todo.input}</p>
+      <div className="todo-buttons">
+        <button
+          onClick={() => {
+            handleCompletetodo(todoIndex);
+          }}
+          disabled={todo.complete}
+        >
+          <h6>Done</h6>
+        </button>
+        <button
+          onClick={() => {
+            handleDeleteTodo(todoIndex);
+          }}
+        >
+          <h6>Delete</h6>
+        </button>
+      </div>
+    </div>
+  );
+}
